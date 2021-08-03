b0b950b4a52455ef4fec2e32d03d6839c3fa2fbb
Hui Wang <hui.wang@bizfocus.cn>
2021-08-03 17:04:20 +0800

    文档集成到README.md

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# rn-element
+## rn-elements
 
 ## install
 ```shell
 npm i rn-element
 ```
 
+## usage docs
+- [form](https://github.com/HuiWang111/rn-element/blob/main/docs/form.md)
+- [list](https://github.com/HuiWang111/rn-element/blob/main/docs/list.md)
+- [loading](https://github.com/HuiWang111/rn-element/blob/main/docs/loading.md)
+- [toast](https://github.com/HuiWang111/rn-element/blob/main/docs/toast.md)
---
+++ scripts/gen-docs.mjs
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+
+const docs = fs.readdirSync(path.join(process.cwd(), 'docs'));
+const baseURL = 'https://github.com/HuiWang111/rn-element/blob/main/docs/';
+let READMEContent = initializeREADME();
+
+docs.forEach(doc => {
+    const fileName = doc.split('.')[0];
+
+    READMEContent += `- [${fileName}](${baseURL}${doc})
+`;
+});
+
+fs.writeFileSync(
+    path.join(process.cwd(), 'README.md'),
+    READMEContent
+)
+
+function initializeREADME(content = '## rn-elements\n') {
+    content += '\n';
+    content += '## install\n';
+    content += '```shell\n';
+    content += 'npm i rn-element\n';
+    content += '```\n';
+    content += '\n';
+    content += '## usage docs\n';
+
+    return content;
+}
+
