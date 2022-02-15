ab301546d3b819870941f73d2b5eb9f0c6893635
kennys_wang <kennys_wang@163.com>
2022-02-15 21:43:27 +0800

    全局存在Loading的情况下阻止键盘事件 代码重写

--- src/hooks/useKeyUp.ts
+++ src/hooks/useKeyUp.ts
@@ -2,7 +2,7 @@
  * @Autor: hui.wang
  * @Date: 2021-08-13 20:31:55
  * @LastEditors: hui.wang
- * @LastEditTime: 2022-02-15 21:25:42
+ * @LastEditTime: 2022-02-15 21:41:02
  * @emial: hui.wang@bizfocus.cn
  */
 /* eslint-disable react-hooks/exhaustive-deps */
@@ -18,19 +18,32 @@ interface IEventType {
 type EventListener = (event: IEventType) => void;
 
 const KeyUpEventName = 'keyup'
+const createListener: (listener: EventListener) => EventListener = (listener) => (e) => {
+    /**
+     * @description: 当前存在Loading的情况下，阻止键盘事件，防止重复提交。
+     */
+    if (Loading.isLoading()) {
+        return
+    }
+
+    listener(e)
+}
+const createListenerWithKeyCode: (callback: () => void, targetKeyCode: number) => EventListener = (callback, targetKeyCode) => (e) => {
+    /**
+     * @description: 当前存在Loading的情况下，阻止键盘事件，防止重复提交。
+     */
+    if (Loading.isLoading() || targetKeyCode !== e.which) {
+        return
+    }
+
+    callback()
+}
 
 export function useKeyUp(listener: EventListener, deps: any[] = []): void {
     useEffect(() => {
-        /**
-         * @description: 当前存在Loading的情况下，阻止键盘事件，防止重复提交。
-         */
-        if (Loading.isLoading()) {
-            return
-        }
-
         let subscription: EmitterSubscription | undefined
         const didMount = () => {
-            subscription = DeviceEventEmitter.addListener(KeyUpEventName, listener)
+            subscription = DeviceEventEmitter.addListener(KeyUpEventName, createListener(listener))
         }
         const willUnmount = () => {
             subscription?.remove()
@@ -44,19 +57,10 @@ export function useKeyUp(listener: EventListener, deps: any[] = []): void {
 
 export function useEnter(callback: () => void, deps: any[] = []): void {
     useEffect(() => {
-        if (Loading.isLoading()) {
-            return
-        }
-
         let subscription: EmitterSubscription | undefined
-        const listener = (e: IEventType) => {
-            if (e.which === KeyCode.Enter) {
-                callback()
-            }
-        }
 
         const didMount = () => {
-            subscription = DeviceEventEmitter.addListener(KeyUpEventName, listener)
+            subscription = DeviceEventEmitter.addListener(KeyUpEventName, createListenerWithKeyCode(callback, KeyCode.Enter))
         }
         const willUnmount = () => {
             subscription?.remove()
@@ -70,19 +74,10 @@ export function useEnter(callback: () => void, deps: any[] = []): void {
 
 export function useArrowUp(callback: () => void, deps: any[] = []): void {
     useEffect(() => {
-        if (Loading.isLoading()) {
-            return
-        }
-
         let subscription: EmitterSubscription | undefined
-        const listener = (e: IEventType) => {
-            if (e.which === KeyCode.Up) {
-                callback()
-            }
-        }
 
         const didMount = () => {
-            subscription = DeviceEventEmitter.addListener(KeyUpEventName, listener)
+            subscription = DeviceEventEmitter.addListener(KeyUpEventName, createListenerWithKeyCode(callback, KeyCode.Up))
         }
         const willUnmount = () => {
             subscription?.remove()
@@ -97,18 +92,8 @@ export function useArrowUp(callback: () => void, deps: any[] = []): void {
 export function useArrowDown(callback: () => void, deps: any[] = []): void {
     let subscription: EmitterSubscription | undefined
     useEffect(() => {
-        if (Loading.isLoading()) {
-            return
-        }
-
-        const listener = (e: IEventType) => {
-            if (e.which === KeyCode.Down) {
-                callback()
-            }
-        }
-
         const didMount = () => {
-            subscription = DeviceEventEmitter.addListener(KeyUpEventName, listener)
+            subscription = DeviceEventEmitter.addListener(KeyUpEventName, createListenerWithKeyCode(callback, KeyCode.Down))
         }
         const willUnmount = () => {
             subscription?.remove()
@@ -123,18 +108,8 @@ export function useArrowDown(callback: () => void, deps: any[] = []): void {
 export function useArrowLeft(callback: () => void, deps: any[] = []): void {
     let subscription: EmitterSubscription | undefined
     useEffect(() => {
-        if (Loading.isLoading()) {
-            return
-        }
-
-        const listener = (e: IEventType) => {
-            if (e.which === KeyCode.Left) {
-                callback()
-            }
-        }
-
         const didMount = () => {
-            subscription = DeviceEventEmitter.addListener(KeyUpEventName, listener)
+            subscription = DeviceEventEmitter.addListener(KeyUpEventName, createListenerWithKeyCode(callback, KeyCode.Left))
         }
         const willUnmount = () => {
             subscription?.remove()
@@ -149,18 +124,8 @@ export function useArrowLeft(callback: () => void, deps: any[] = []): void {
 export function useArrowRight(callback: () => void, deps: any[] = []): void {
     let subscription: EmitterSubscription | undefined
     useEffect(() => {
-        if (Loading.isLoading()) {
-            return
-        }
-
-        const listener = (e: IEventType) => {
-            if (e.which === KeyCode.Right) {
-                callback()
-            }
-        }
-
         const didMount = () => {
-            subscription = DeviceEventEmitter.addListener(KeyUpEventName, listener)
+            subscription = DeviceEventEmitter.addListener(KeyUpEventName, createListenerWithKeyCode(callback, KeyCode.Right))
         }
         const willUnmount = () => {
             subscription?.remove()
